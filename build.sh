686404154b51edea672a6e4c426821b8dfd40c6f
Domakonda SaiSharan <99661288+saisharan0103@users.noreply.github.com>
2025-05-21 17:52:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
  
 # Install uv
-curl -Ls https://astral.sh/uv/install.sh | sh
+#curl -Ls https://astral.sh/uv/install.sh | sh
  
 # Add uv to PATH if needed (Render may not auto-load it)
-export PATH="$HOME/.local/bin:$PATH"
+#export PATH="$HOME/.local/bin:$PATH"
  
 # Run your editable install
 uv pip install -e .
+
+which uvx
